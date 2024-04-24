b146cf8480ce449215a3cb8530fce1bcc3339a10
gentlandy <gentlandy16@gmail.com>
2024-04-24 18:21:22 +0700

    Initialize commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Landy on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
