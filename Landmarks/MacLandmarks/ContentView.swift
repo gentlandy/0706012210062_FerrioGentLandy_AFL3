302edcea8750acb25b9be1bc0949b8bc78f0e95d
gentlandy <gentlandy16@gmail.com>
2024-04-27 01:37:22 +0700

    Chapter 4

---
+++ Landmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Landy on 26/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+#Preview {
+    ContentView()
+        .environment(ModelData())
+}
